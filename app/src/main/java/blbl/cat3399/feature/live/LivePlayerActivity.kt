5bb2e502d9f130df93703eb253ac0737528b1a39
cat3399 <1499276721hu@gmail.com>
2026-01-24 01:24:12 +0800

    去掉不恰当的按钮

--- app/src/main/java/blbl/cat3399/feature/live/LivePlayerActivity.kt
+++ app/src/main/java/blbl/cat3399/feature/live/LivePlayerActivity.kt
@@ -102,6 +102,7 @@ class LivePlayerActivity : AppCompatActivity() {
         binding.btnLike.visibility = View.GONE
         binding.btnCoin.visibility = View.GONE
         binding.btnFav.visibility = View.GONE
+        binding.btnRecommend.visibility = View.GONE
 
         binding.btnBack.setOnClickListener { finish() }
 
